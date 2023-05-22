8329ab4e56d7d4998862ce4a95e92aad603696e6
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-05-22 05:53:50 -0700

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",20,"M","red","sanguine",1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy",21,"F","orange","choleric",0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha",22,"M","yellow","melancholy",1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",23,"F","green","phlegmatic",0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",20,"M","red","sanguine",1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES("Grinch",21,"F","orange","choleric",0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES("Wendy",22,"M","yellow","melancholy",1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES(NULL,23,"F","green","phlegmatic",0)
